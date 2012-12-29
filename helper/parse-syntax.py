1e959019c1da2427e2efb806a25181b3b064714d
Mosky <mosky.tw@gmail.com>
2012-12-30 03:14:12 +0800

    rewrited parse-syntax.py

--- helper/parse-syntax.py
+++ helper/parse-syntax.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-syntax_txt = open('syntax.txt').read()
-
 import re
 
-re_groups = re.compile('^((?:[A-Z][a-z]+)+)\t+', re.M)
+to_collect_patterns = iter([
+    (
+        re.compile("\*Comment"),
+        re.compile("\*Todo"),
+        re.compile("((?:[A-Z][A-Za-z]+)+)"),
+    ),
+    (
+        re.compile("ColorColumn\tused for the columns set with 'colorcolumn'"),
+        re.compile("Tooltip\t\tCurrent font, background and foreground of the tooltips."),
+        re.compile("^((?:[A-Z][A-Za-z]+)+)"),
+    ),
+])
 
-c = 0
-for m in re_groups.finditer(syntax_txt):
-    c += 1
-    print m.group(1)
-print
-print 'count:', c
-print
+start, end, pattern = next(to_collect_patterns)
 
-re_conventions = re.compile('^\s{2,}\*?((?:[A-Z][a-z]+)+)', re.M)
+to_collect = False
+word_list = []
+word_lists = []
 
-c = 0
-for m in re_conventions.finditer(syntax_txt):
-    c += 1
-    print m.group(1)
+for i, line in enumerate(open('syntax.txt')):
+
+    if start.search(line):
+        to_collect = True
+
+    if not to_collect: continue
+
+    match = pattern.search(line)
+    if match: word_list.append(match.group(1))
+
+    if end.search(line):
+        to_collect = False
+        word_lists.append(word_list)
+        word_list = []
+        try:
+            start, end, pattern = next(to_collect_patterns)
+        except StopIteration:
+            break
+
+print 'CONVENTIONAL_GROUP_NAMES = %r' % word_lists[0]
 print
-print 'count:', c
+print 'VIM_GROUP_NAMES = %r' % word_lists[1]
+print
+print 'count: ', map(len, word_lists)
